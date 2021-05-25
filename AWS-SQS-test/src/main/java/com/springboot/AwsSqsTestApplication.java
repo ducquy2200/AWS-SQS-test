f2b996d650c1d0bd14795995eba4c92d0f13195a
ducquy2200 <ducquy2200@gmail.com>
2021-05-25 19:34:07 -0400

    upload files

---
+++ AWS-SQS-test/src/main/java/com/springboot/AwsSqsTestApplication.java
@@ -0,0 +1,40 @@
+package com.springboot;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = {ContextStackAutoConfiguration.class})
+@RestController
+public class AwsSqsTestApplication {
+	
+	@Autowired
+	private QueueMessagingTemplate queueMessagingTemplate;
+	
+	@Value("${cloud.aws.end-point.uri}")
+	private String endpoint;
+	
+	@GetMapping("/message/{message}")
+	public void sendMessage(@PathVariable String message) {
+		queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(message).build());
+	}
+	
+	@SqsListener("AWS-SQS-test")
+	public void receiveMessage(String message) {
+		LoggerFactory.getLogger(AwsSqsTestApplication.class).info("Message: \"{}\"", message);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSqsTestApplication.class, args);
+	}
+
+}
